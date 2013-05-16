2595fdb4069ee2c05486d2e770c2958ba715f43a
Rory Standley <rorystandley@gmail.com>
2013-05-16 21:25:29 +0100

    Create Validation Filters

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ PHP-Form-Validation
 ===================
 
 PHP HTML Form validation
+
+Currently checks to see if a name, email and url field have been populated. If they have it will then perform a check to see if the values, indeed suit the criteria of such an input.
--- process.php
+++ process.php
@@ -17,36 +17,47 @@ $requiredFields = array(
 );
 // Loop through the $_POST array, which comes from the form...
 $errors = array();
-foreach($_POST AS $key => $value)
-{
+foreach($_POST AS $key => $value){
     // first need to make sure this is an allowed field
-    if(in_array($key, $allowedFields))
-    {
+    if(in_array($key, $allowedFields)){
         $$key = $value;
-         
         // is this a required field?
-        if(in_array($key, $requiredFields) && $value == '')
-        {
+        if(in_array($key, $requiredFields) && $value == ''){
             $errors[] = "The field $key is required.";
+        }else{
+        	// The field was populated, lets check to see if its valid
+        	if($key == "email"){
+        		if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+        			// Not valid add to errors
+        			$errors[] = "Please enter a valid $key";
+        		}else{
+        			// Valid lets sanitize the input
+        			$_POST["email"] = filter_var($_POST["email"],FILTER_SANITIZE_EMAIL);
+        		}
+        	}else if($key == "name"){
+        		$_POST["name"] = filter_var($_POST["name"],FILTER_SANITIZE_STRING);
+        	}else if($key == "url"){
+        		if(!filter_var($_POST["url"],FILTER_VALIDATE_URL)){
+        			$errors[] = "Please enter a valid $key, try adding http://";
+        		}else{
+        			$_POST["url"] = filter_var($_POST["url"],FILTER_SANITIZE_URL);
+        		}
+        	}
         }
     }  
 }
 // were there any errors?
-if(count($errors) > 0)
-{
+if(count($errors) > 0){
     $errorString = '<p>There was an error processing the form.</p>';
     $errorString .= '<ul>';
-    foreach($errors as $error)
-    {
+    foreach($errors as $error){
         $errorString .= "<li>$error</li>";
     }
     $errorString .= '</ul>';
      
     // display the previous form
     include 'index.php';
-}
-else
-{
+}else{
     // At this point you can send out an email or do whatever you want
     // with the data...
      
